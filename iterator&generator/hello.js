6884b2e871b8f184d0c3fe83fe2ab5116d402e7c
Tohidul Islam <mdtohid.sust@gmail.com>
2021-04-12 01:06:19 +0600

    Objecter er vetor e iterator nia ektu  moja korlam

--- iterator&generator/hello.js
+++ iterator&generator/hello.js
@@ -1,10 +1,10 @@
 var obj = { a: 10, b: 20, c: 30 };
-let entries = Object.entries(obj);
-let length = entries.length;
+// let entries = Object.values(obj);
+// let length = entries.length;
 
 Object.prototype[Symbol.iterator] = function () {
-  //   let entries = Object.entries(this);
-  //   let length = entries.length;
+  let entries = Object.entries(this);
+  let length = entries.length;
   let i = 0;
   return {
     next() {
@@ -16,12 +16,14 @@ Object.prototype[Symbol.iterator] = function () {
   };
 };
 let iterate = obj[Symbol.iterator]();
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
-console.log(iterate.next());
+console.log(iterate.next()); //[ 'a', 10 ]
+console.log(iterate.next()); //[ 'b', 20 ]
+console.log(iterate.next()); //[ 'c', 30 ]
+console.log(iterate.next()); //undefimed
+console.log(iterate.next()); //undefimed
 
-for (let pair of obj) {
-  console.log(pair);
+let arr = [...obj]; //[ [ 'a', 10 ], [ 'b', 20 ], [ 'c', 30 ] ]
+console.log(arr); //[ [ 'a', 10 ], [ 'b', 20 ], [ 'c', 30 ] ]
+for (let value of obj) {
+  console.log(value); //[ 'a', 10 ] [ 'b', 20 ] [ 'c', 30 ]
 }
--- iterator&generator/iterator_generator.js
+++ iterator&generator/iterator_generator.js
@@ -1,4 +1,4 @@
-////Custom build iterator/////////
+////Custom build iterator(nayem)/////////
 // var arr = [10, 22, 35]
 
 // function createIterator(collection) {
@@ -22,7 +22,7 @@
 // console.log(iterate.next());
 // console.log(iterate.next());
 
-////////////////////////////////custom iterator 2////////////////////////////////
+////////////////////////////////custom iterator 2(sumit)////////////////////////////////
 // function range(start, end, step) {
 //   let current = start;
 //   return {
@@ -41,6 +41,71 @@
 //   };
 // }
 // console.log(...range(1, 50000, 2));
+
+
+////////////////////////////////custom iterator 3(sumit)////////////////////////////////
+// var obj = { a: 10, b: 20, c: 30 };
+// let entries = Object.values(obj);
+// let length = entries.length;
+
+// Object.prototype[Symbol.iterator] = function () {
+//   //   let entries = Object.entries(this);
+//   //   let length = entries.length;
+//   let i = 0;
+//   return {
+//     next() {
+//       return {
+//         done: i >= length,
+//         value: entries[i++],
+//       };
+//     },
+//   };
+// };
+// let iterate = obj[Symbol.iterator]();
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.log(iterate.next());
+
+// let arr = [...obj]; //[ 10, 20, 30 ]
+// console.log(arr); //[ 10, 20, 30 ]
+// for (let value of obj) {
+//   console.log(value);//10 20 30
+
+// }
+
+////////////////////////////////custom iterator 4(sumit)////////////////////////////////
+// var obj = { a: 10, b: 20, c: 30 };
+// // let entries = Object.values(obj);
+// // let length = entries.length;
+
+// Object.prototype[Symbol.iterator] = function () {
+//   let entries = Object.entries(this);
+//   let length = entries.length;
+//   let i = 0;
+//   return {
+//     next() {
+//       return {
+//         done: i >= length,
+//         value: entries[i++],
+//       };
+//     },
+//   };
+// };
+// let iterate = obj[Symbol.iterator]();
+// console.log(iterate.next()); //[ 'a', 10 ]
+// console.log(iterate.next()); //[ 'b', 20 ]
+// console.log(iterate.next()); //[ 'c', 30 ]
+// console.log(iterate.next()); //undefimed
+// console.log(iterate.next()); //undefimed
+
+// let arr = [...obj]; //[ [ 'a', 10 ], [ 'b', 20 ], [ 'c', 30 ] ]
+// console.log(arr); //[ [ 'a', 10 ], [ 'b', 20 ], [ 'c', 30 ] ]
+// for (let value of obj) {
+//   console.log(value); //[ 'a', 10 ] [ 'b', 20 ] [ 'c', 30 ]
+// }
+
 
 
 
