259d52bb9e47c700c361a3fa0ec591d5bb7617a9
Tohidul Islam <mdtohid.sust@gmail.com>
2021-05-12 19:47:48 +0600

    used promise for requesting

--- AJAX/ajax-1.js
+++ AJAX/ajax-1.js
@@ -1,23 +1,25 @@
-const getRequest = (url, callback) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  xhr.send();
-
-  xhr.onreadystatechange = (e) => {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        const users = JSON.parse(xhr.response);
-        callback(null, users);
-      } else {
-        callback(xhr.status, null);
+const getRequest = (url) => {
+  return (promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.send();
+    xhr.onreadystatechange = (e) => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          const users = JSON.parse(xhr.response);
+          resolve(users);
+        } else {
+          reject(xhr.status);
+        }
       }
-    }
-  };
+    };
+  }));
 };
-getRequest("https://jsonplaceholder.typicode.com/todos", (err, res) => {
-  if (err) {
+
+getRequest("https://jsonplaceholder.typicode.com/todos")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
     console.log(err);
-  } else {
-    res.forEach((res) => console.log(res.title));
-  }
-});
+  });
