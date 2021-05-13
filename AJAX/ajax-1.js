69e7ccf283dd9d48cb4522de686f8aa8f6af5da2
Tohidul Islam <mdtohid.sust@gmail.com>
2021-05-13 18:02:02 +0600

    asynch await with fetch api

--- AJAX/ajax-1.js
+++ AJAX/ajax-1.js
@@ -1,21 +1,7 @@
-function getData(url) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.send();
-    xhr.onload = () => {
-      if (xhr.response) {
-        resolve(JSON.parse(xhr.response));
-      } else {
-        reject("Failed!");
-      }
-    };
-  });
-}
-
 async function asyncFunction(url) {
-  const res = await getData(url);
-  console.log(res);
+  const res1 = await fetch(url);
+  const res2 = await res1.json();
+  console.log(res2);
 }
 
 asyncFunction("https://jsonplaceholder.typicode.com/todos/1");
