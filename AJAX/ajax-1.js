1c22a12bb4d7e6319d8787305b0600746ff8528f
Tohidul Islam <mdtohid.sust@gmail.com>
2021-05-13 18:27:07 +0600

    asynch await with promise all

--- AJAX/ajax-1.js
+++ AJAX/ajax-1.js
@@ -1,7 +1,17 @@
-async function asyncFunction(url) {
-  const res1 = await fetch(url);
-  const res2 = await res1.json();
+const promiseArray = [
+  fetch("https://jsonplaceholder.typicode.com/posts"),
+  fetch("https://jsonplaceholder.typicode.com/posts/10"),
+  fetch("https://jsonplaceholder.typicode.com/posts/3"),
+];
+
+async function asyncFunction(arr) {
+  const res1 = await Promise.all(arr);
+  const res2 = await Promise.all(
+    res1.map((v) => {
+      return v.json();
+    })
+  );
   console.log(res2);
 }
 
-asyncFunction("https://jsonplaceholder.typicode.com/todos/1");
+asyncFunction(promiseArray);
